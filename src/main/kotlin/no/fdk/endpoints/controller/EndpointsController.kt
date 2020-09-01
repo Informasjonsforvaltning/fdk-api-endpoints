293b05a7abf18a2011ce2a000e8ce61f86e1b2de
NilsOveTen <nils.ove.tendenes@acando.no>
2020-09-01 13:06:50 +0200

    chore: initial app

---
+++ src/main/kotlin/no/fdk/endpoints/controller/EndpointsController.kt
@@ -0,0 +1,32 @@
+package no.fdk.endpoints.controller
+
+import no.fdk.endpoints.model.Endpoint
+import no.fdk.endpoints.model.Environment
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+private val logger = LoggerFactory.getLogger(EndpointsController::class.java)
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = ["/endpoints"])
+class EndpointsController {
+
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun searchAPICatalog(
+        @RequestParam(value = "environment", required = true) environment: Environment,
+        @RequestParam(value = "activeOnly", required = false, defaultValue = "true") activeOnly: Boolean,
+        @RequestParam(value = "serviceType", required = false) serviceType: String?,
+        @RequestParam(value = "orgNos", required = false) orgNos: List<String>?
+    ): ResponseEntity<List<Endpoint>> {
+        return ResponseEntity(HttpStatus.NOT_IMPLEMENTED)
+    }
+
+}
